8667b69364bfaa5e3b30bfb5f569cf46b2adba2d
elbukaevzaur <elbukaevzaur@gmail.com>
2021-08-13 19:34:46 +0300

    init app

---
+++ src/main/java/com/beginend/grozmerbackend/GrozmerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beginend.grozmerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrozmerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrozmerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beginend/grozmerbackend/GrozmerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beginend.grozmerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrozmerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
